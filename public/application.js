b54c569079f2627c83028f3612a2b610523e153e
KOSASIH <kosasihg88@gmail.com>
2024-05-31 12:45:11 +0700

    Create application.js

---
+++ public/application.js
@@ -0,0 +1,9 @@
+// application.js
+//= require jquery
+//= require bootstrap
+//= require chart
+//= require datatable
+
+$(document).ready(function() {
+  // Initialize JavaScript plugins and scripts here
+});
