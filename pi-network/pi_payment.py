fc2e62bacc647cc2187d31dc9447054d98622be7
KOSASIH <kosasihg88@gmail.com>
2024-05-31 14:38:41 +0700

    Create pi_payment.py

---
+++ pi-network/pi_payment.py
@@ -0,0 +1,12 @@
+from.pi_wallet import PiWallet
+
+class PiPayment:
+    def __init__(self, username, password):
+        self.pi_wallet = PiWallet(username, password)
+
+    def make_payment(self, recipient_username, amount):
+        balance = self.pi_wallet.get_balance()
+        if balance >= amount:
+            self.pi_wallet.send_pi(recipient_username, amount)
+            return True
+        return False
