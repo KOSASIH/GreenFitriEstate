4556f3a9d9de3015c807efdf4fdf9a272fa4ee2f
KOSASIH <kosasihg88@gmail.com>
2024-05-31 14:40:58 +0700

    Create views.py

---
+++ app/views/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from.forms import PiPaymentForm
+from pi_network.pi_payment import PiPayment
+
+def pi_payment_view(request):
+    if request.method == "POST":
+        form = PiPaymentForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            recipient_username = form.cleaned_data["recipient_username"]
+            amount = form.cleaned_data["amount"]
+            pi_payment = PiPayment(username, password)
+            if pi_payment.make_payment(recipient_username, amount):
+                return redirect("payment_success")
+            else:
+                return redirect("payment_failure")
+    else:
+        form = PiPaymentForm()
+    return render(request, "pi_payment_form.html", {"form": form})
